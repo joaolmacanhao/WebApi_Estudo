18402ecdddb954ddf0bd030db0e8494d6ae496cb
joaolmacanhao <joao.macanhao@ufms.br>
2025-10-18 08:31:57 -0400

    Criacao dos contratros da interface IFuncionarioInterface

--- WebApi_Estudo/Service/FuncionarioService/FuncionarioService.cs
+++ WebApi_Estudo/Service/FuncionarioService/FuncionarioService.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApi_Estudo.Service.FuncionarioService
+﻿using WebApi_Estudo.Models;
+
+namespace WebApi_Estudo.Service.FuncionarioService
 {
     public class FuncionarioService : IFuncionarioInterface
     {
+        public Task<ServiceResponse<List<FuncionarioModel>>> CreatFucionarios(FuncionarioModel novoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> DeleteFucionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> GetFucionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionarioById(FuncionarioModel editandoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WebApi_Estudo/Service/FuncionarioService/IFuncionarioInterface.cs
+++ WebApi_Estudo/Service/FuncionarioService/IFuncionarioInterface.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApi_Estudo.Service.FuncionarioService
+﻿using WebApi_Estudo.Models;
+
+namespace WebApi_Estudo.Service.FuncionarioService
 {
     public interface IFuncionarioInterface
     {
+
+        Task<ServiceResponse<List<FuncionarioModel>>> GetFucionarios();
+        Task<ServiceResponse<List<FuncionarioModel>>> CreatFucionarios(FuncionarioModel novoFuncionario);
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>>UpdateFuncionarioById(FuncionarioModel editandoFuncionario);
+        Task<ServiceResponse<List<FuncionarioModel>>>DeleteFucionarioById(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id);
     }
 }
